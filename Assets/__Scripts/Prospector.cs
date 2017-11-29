e1e8dedd6237023e3196da5b40b7c17cdc030808
danicarose <danica.calusin@gmail.com>
2017-11-28 19:25:05 -0500

    began working w CardProspector, some issues

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -17,16 +17,30 @@ public class Prospector : MonoBehaviour {
 		S = this;
 	}
 
+	public List<CardProspector> drawPile;
+
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 
-		Deck.Shuffle (ref deck.cards);
+		Deck.Shuffle (ref deck.cards); //should i comment this out?
 		//The ref keyword passes a reference to deck.cards, which allows
 		// deck.cards to be modified by Deck.Shuffle()
 
 		layout = GetComponent<Layout> ();
 		layout.ReadLayout (layoutXML.text);
+
+		drawPile = ConvertListCardsToListCardProspectors (deck.cards);
+	}
+
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD){
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach (Card tCD in lCD) {
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return(lCP);
 	}
 
 }
